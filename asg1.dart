2d5ef2c83dbbf5335c753b297b6bc249a75401cf
unknown <reganjjs@gmail.com>
2023-03-08 09:46:03 +0300

    first commit

---
+++ asg1.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  int age = 25;
+  double cash = 7.8;
+  String name = 'Regan';
+  String intro = "Hi I'm ";
+  List siblings = ['brothers', 'sisters'];
+  Map siblingInTotal = {'brothers': 2, 'sisters': 1};
+
+  //print introduction and age
+  print('$intro $name, i have $age years old');
+  //print the amount you have
+  print('$name has $cash Tsh');
+  // print siblings
+  print('these are my siblings $siblingInTotal');
+}
